2f24851d58f45d10d69290c53a971e242ae09c21
Anthony D. Dutoi <adutoi@indra>
2020-10-29 12:18:41 -0700

    external TonyUtil hooked up and tests pass

---
+++ pytoon/util/external/__init__.py
@@ -0,0 +1,21 @@
+#  (C) Copyright 2020 Anthony D. Dutoi
+#
+#  This file is part of PyToon.
+#
+#  PyToon is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from .TonyUtil.util import struct, as_tuple, as_dict, int_round, named_colors, shell, code_template
+from .TonyUtil.util import svg        as svg_code
+from .TonyUtil.util import javascript as  js_code
