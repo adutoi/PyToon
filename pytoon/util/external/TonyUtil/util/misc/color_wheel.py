38b3c177a6bb166d76c2fed4d495bf7a2a7819dc
Anthony D. Dutoi <adutoi@tengri>
2020-12-03 13:22:48 -0800

    color wheel tweaked

--- pytoon/util/external/TonyUtil/util/misc/color_wheel.py
+++ pytoon/util/external/TonyUtil/util/misc/color_wheel.py
@@ -22,58 +22,73 @@ from .int_round import int_round
 
 def color_wheel(phi):
     """ maps angle (in radians) to rainbow color wheel, optimized to have constant brightness (can always be improved) """
-    red    = 0xcc, 0x33, 0x33
-    orange = 0xcc, 0x55, 0
-    yellow = 0x99, 0x88, 0
-    green  = 0,    0x77, 0x11
-    blue   = 0x66, 0x77, 0xff
-    purple = 0xbb, 0x11, 0xee
+    color = {}
+    color["red"]    = 0xee, 0x33, 0x33
+    color["orange"] = 0xdd, 0x55, 0
+    color["yellow"] = 0x88, 0x88, 0
+    color["green"]  = 0x22, 0x99, 0x22
+    color["blue"]   = 0x33, 0x88, 0xff
+    color["purple"] = 0xcc, 0x44, 0xee
+    position ={}
+    position["red1"]   = -1
+    position["orange"] = -2/3 - 0.1
+    position["yellow"] = -1/3 + 0.05
+    position["green"]  =  0
+    position["blue"]   = +1/3 - 0.1
+    position["purple"] = +2/3 - 0.05
+    position["red2"]   = +1
     phi /= math.pi
-    if   phi<-2/3:
-        w = 3*(phi + 1)
+    if   phi<position["orange"]:
+        D = position["orange"] - position["red1"]
+        w = (phi - position["red1"]) / D
         v = 1 - w
-        r1, g1, b1 = red
-        r2, g2, b2 = orange
+        r1, g1, b1 = color["red"]
+        r2, g2, b2 = color["orange"]
         rr = int_round(v*r1 + w*r2)
         gg = int_round(v*g1 + w*g2)
         bb = int_round(v*b1 + w*b2)
-    elif phi<-1/3:
-        w = 3*(phi + 2/3)
+    elif phi<position["yellow"]:
+        D = position["yellow"] - position["orange"]
+        w = (phi - position["orange"]) / D
         v = 1 - w
-        r1, g1, b1 = orange
-        r2, g2, b2 = yellow
+        r1, g1, b1 = color["orange"]
+        r2, g2, b2 = color["yellow"]
         rr = int_round(v*r1 + w*r2)
         gg = int_round(v*g1 + w*g2)
         bb = int_round(v*b1 + w*b2)
-    elif phi< 0:
-        w = 3*(phi + 1/3)
+    elif phi<position["green"]:
+        D = position["green"] - position["yellow"]
+        w = (phi - position["yellow"]) / D
         v = 1 - w
-        r1, g1, b1 = yellow
-        r2, g2, b2 = green
+        r1, g1, b1 = color["yellow"]
+        r2, g2, b2 = color["green"]
         rr = int_round(v*r1 + w*r2)
         gg = int_round(v*g1 + w*g2)
         bb = int_round(v*b1 + w*b2)
-    elif phi< 1/3:
-        w = 3*phi
+    elif phi<position["blue"]:
+        D = position["blue"] - position["green"]
+        w = (phi - position["green"]) / D
         v = 1 - w
-        r1, g1, b1 = green
-        r2, g2, b2 = blue
+        r1, g1, b1 = color["green"]
+        r2, g2, b2 = color["blue"]
         rr = int_round(v*r1 + w*r2)
         gg = int_round(v*g1 + w*g2)
         bb = int_round(v*b1 + w*b2)
-    elif phi< 2/3:
-        w = 3*(phi - 1/3)
+    elif phi<position["purple"]:
+        D = position["purple"] - position["blue"]
+        w = (phi - position["blue"]) / D
         v = 1 - w
-        r1, g1, b1 = blue
-        r2, g2, b2 = purple
+        r1, g1, b1 = color["blue"]
+        r2, g2, b2 = color["purple"]
         rr = int_round(v*r1 + w*r2)
         gg = int_round(v*g1 + w*g2)
         bb = int_round(v*b1 + w*b2)
     else:
-        w = 3*(phi - 2/3)
+        D = position["red2"] - position["purple"]
+        w = (phi - position["purple"]) / D
         v = 1 - w
-        r1, g1, b1 = purple
-        r2, g2, b2 = red
+        r1, g1, b1 = color["purple"]
+        r2, g2, b2 = color["red"]
         rr = int_round(v*r1 + w*r2)
         gg = int_round(v*g1 + w*g2)
         bb = int_round(v*b1 + w*b2)
