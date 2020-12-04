08ce57d472ad998315c573932449968c150a09e1
Anthony D. Dutoi <adutoi@indra>
2020-12-04 01:26:10 -0800

    synced TonyUtil again

--- pytoon/util/external/TonyUtil/util/external/read_input.py
+++ pytoon/util/external/TonyUtil/util/external/read_input.py
@@ -15,6 +15,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with TonyUtil.  If not, see <http://www.gnu.org/licenses/>.
 #
+from .misc.struct import struct
 
 # This was stolen from my Qode project, where a copy still lives figure out how to get Hg to handle dependencies!
 
@@ -42,40 +43,30 @@ does not like . . . try-except is also probably my friend in this battle.
 
 
 
-class _dotdict(dict):    # replace with SimpleNamespace?
-    """\
-    dot.notation access to dictionary attributes
-    Found at http://stackoverflow.com/questions/2352181/how-to-use-a-dot-to-access-members-of-dictionary,
-    on 21.May.2015 courtesy of 'derek73'.
-    """
-    def __getattr__(self,attr):  return self.get(attr)
-    __setattr__= dict.__setitem__
-    __delattr__= dict.__delitem__
+#class _dotdict(dict):    # replace with SimpleNamespace?
+#    """\
+#    dot.notation access to dictionary attributes
+#    Found at http://stackoverflow.com/questions/2352181/how-to-use-a-dot-to-access-members-of-dictionary,
+#    on 21.May.2015 courtesy of 'derek73'.
+#    """
+#    def __getattr__(self,attr):  return self.get(attr)
+#    __setattr__= dict.__setitem__
+#    __delattr__= dict.__delitem__
 
 
 
-def from_string(string,write_to=None,namespace=None):
-    """\
-    Takes a string containing python code and executes it using the global namespace given as an argument
-    (to expose names in modules like math, without them having to be referenced or imported in the string).
-    The names defined inside the executed string are assigned to a namespace which is returned to the user.
-    The identfiers in this space may be accessed using the . notation customary for builtin dictionaries.
-    """
+def from_string(string, namespace=None):
+    """ Executes a code string using the namespace given, returning names defined therein in a struct """
     if namespace is None:  namespace = globals()
-    if write_to  is None:  write_to  = _dotdict({})
-    exec(string,namespace,write_to)
-    return write_to
+    write_to = {}
+    exec(string, namespace, write_to)
+    return struct(**write_to)
 
-def from_file(filename,write_to=None,namespace=None):
-    """\
-    Just a wrapper for read_input.from_string, but where the contents of an ASCII file are read as the string.
-    """
-    return from_string(open(filename).read(),write_to,namespace)
+def from_file(filename, namespace=None):
+    """ Just a wrapper for read_input.from_string, but using the contents of an ASCII file """
+    return from_string(open(filename).read(), namespace)
 
-def from_argv(fields,write_to=None,namespace=None):
-    """\
-    Just a wrapper for read_input.from_string, but where a list of statements is executed (such as would be found in argv with no space around = signs)
-    """
-    string = ""
-    for field in fields:  string += field + '\n'
-    return from_string(string,write_to,namespace)
+def from_argv(fields, namespace=None):
+    """ Just a wrapper for read_input.from_string, but where a list of statements is executed (as in sys.argv with no space around = signs) """
+    string = "\n".join(fields)
+    return from_string(string, namespace)
